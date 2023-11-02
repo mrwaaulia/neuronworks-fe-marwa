82b42751f450108162dff378b336c859fc8daf1e
mrwaaulia <111261372+mrwaaulia@users.noreply.github.com>
2023-11-02 14:00:45 +0700

    edit:functional header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,44 +1,112 @@
-import React from "react";
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
 
 function Header() {
+  const [currentStyle, setCurrentStyle] = useState({
+    className: "py-8 bg-white",
+    iconColor: "white",
+    logoColor: ["#ED2024", "#8B181A", "black"],
+  });
+
+  useEffect(() => {
+    const onPageScroll = () => {
+      setCurrentStyle(
+        window.pageYOffset > 16
+          ? {
+              className: "py-6 bg-white/90 shadow-lg",
+              iconColor: "#D61924",
+              logoColor: ["#ED2024", "#8B181A", "black"],
+            }
+          : {
+              className: "py-8 bg-white",
+              iconColor: "white",
+              logoColor: ["#ED2024", "#8B181A", "black"],
+            }
+      );
+    };
+    window.addEventListener("scroll", onPageScroll);
+
+    return () => {
+      window.removeEventListener("scroll", onPageScroll);
+    };
+  }, []);
+
   return (
-    <div className="fixed top-0 inset-x-0 flex items-center justify-between py-8 px-16 bg-white z-20">
-      <div className="flex gap-8">
-        <a href="/">
-          <img src="/assets/logo.svg" alt="Logo" width={200} height={32} />
-        </a>
-        <select>
-          <option value="">EN</option>
-          <option value="">IDN</option>
-        </select>
-      </div>
-      <div className="text-16 flex gap-8">
-        <a href="/" className="hover:text-[#D61924] duration-300 ease-linear">
-          Home
-        </a>
-        <a href="/" className="hover:text-[#D61924] duration-300 ease-linear">
-          About
-        </a>
-        <a href="/" className="hover:text-[#D61924] duration-300 ease-linear">
-          Portfolio
-        </a>
-        <a href="/" className="hover:text-[#D61924] duration-300 ease-linear">
-          Product
-        </a>
-        <a href="/" className="hover:text-[#D61924] duration-300 ease-linear">
-          Meethodology
-        </a>
-        <a href="/" className="hover:text-[#D61924] duration-300 ease-linear">
-          Article
-        </a>
-        <a href="/" className="hover:text-[#D61924] duration-300 ease-linear">
-          Galery
-        </a>
-        <a href="/" className="hover:text-[#D61924] duration-300 ease-linear">
-          Career
-        </a>
-      </div>
-    </div>
+    <section id="header">
+      <header
+        className={`flex justify-between items-center fixed top-0 inset-x-0 px-16 z-30 ${currentStyle.className} duration-300 ease-linear`}
+        style={{ backdropFilter: "blur(5px)" }}
+      >
+        <div className="flex justify-between items-center gap-12">
+          <Link href="/">
+            <Image src="/assets/logo.svg" alt="agus" width={200} height={30} />
+          </Link>
+          <div className="flex items-center gap-8">
+            <select className="bg-transparent w-12 cursor-pointer border-none outline-none">
+              <option className="text-black" value="EN">
+                EN
+              </option>
+              <option className="text-black" value="EN">
+                ID
+              </option>
+            </select>
+          </div>
+        </div>
+        <div className="flex gap-6">
+          <Link
+            href="/"
+            className="hover:text-[#D61924] duration-300 ease-linear"
+          >
+            Home
+          </Link>
+          <Link
+            href="/about"
+            className="hover:text-[#D61924] duration-300 ease-linear"
+          >
+            About
+          </Link>
+          <Link
+            href="/portfolio"
+            className="hover:text-[#D61924] duration-300 ease-linear"
+          >
+            Portfolio
+          </Link>
+          <Link
+            href="/product"
+            className="hover:text-[#D61924] duration-300 ease-linear"
+          >
+            Product
+          </Link>
+          <Link
+            href="/methodology"
+            className="hover:text-[#D61924] duration-300 ease-linear"
+          >
+            Methodology
+          </Link>
+          <Link
+            href="/article"
+            className="hover:text-[#D61924] duration-300 ease-linear"
+          >
+            Article
+          </Link>
+          <Link
+            href="/gallery"
+            className="hover:text-[#D61924] duration-300 ease-linear"
+          >
+            Gallery
+          </Link>
+          <Link
+            href="/career"
+            className="hover:text-[#D61924] duration-300 ease-linear"
+          >
+            Career
+          </Link>
+        </div>
+      </header>
+    </section>
   );
 }
 
